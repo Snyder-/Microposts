ec6b06e7138b85a455d8b2ec29a6a821771f9b7a
Sean M. Snyder <sean.snyder@snhu.edu>
2014-02-15 17:47:04 -0500

    Reconfigured database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140213145228) do
+ActiveRecord::Schema.define(version: 20140215222348) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20140213145228) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "user_id"
   end
 
 end

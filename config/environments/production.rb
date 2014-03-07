3b47cb22c07212174793c73a6bb2449b29767c7a
Sean M. Snyder <sean.snyder@snhu.edu>
2014-03-07 08:30:02 -0500

    fixes for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 Wineclub::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # Log detail is configurable on the server
-  config.log_level = ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].to_sym : ('info').to_sym
   # Code is not reloaded between requests.
   config.cache_classes = true
 

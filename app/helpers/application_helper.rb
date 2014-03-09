2a3673efe01be8d0e16b8f8fc9014de67b201a46
Sean M. Snyder <sean.snyder@snhu.edu>
2014-03-09 11:08:43 -0400

    fixed page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def title(title)
+    content_for(:title, title)
+  end
+  def description(description)
+    content_for(:description, description)
+  end
+  def keywords(keywords)
+    content_for(:keywords, keywords)
+  end
 end

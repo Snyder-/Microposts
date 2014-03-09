7a456497a8b9530409e5df7a0bd45eab3b680fb2
Sean M. Snyder <sean.snyder@snhu.edu>
2014-03-09 16:37:58 -0400

    css fixes/email defaults

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,13 @@ Wineclub::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #Sending email on the production side
+  config.action_mailer.default_url_options = { :host => 'sms-micropost.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address => "127.0.0.1",
+      :port    => 1025,
+      :domain  => 'comcast.net'
+  }
 end

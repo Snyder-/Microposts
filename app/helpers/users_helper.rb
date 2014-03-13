40f1c5e37754d7e8d5bf0006083cd5430a63623d
Sean M. Snyder <sean.snyder@snhu.edu>
2014-03-13 12:59:39 -0400

    bootstrap fixes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,6 +8,11 @@ module UsersHelper
   def mini_gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, size: '35x35', alt: user.name, class: "img-circle")
+    image_tag(gravatar_url, size: '20x16', alt: user.name, class: "img-circle")
+  end
+  def med_gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, size: '30x30', alt: user.name, class: "img-circle")
   end
 end

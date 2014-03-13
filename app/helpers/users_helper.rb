25b9b40cec37c47b474f4f16fc52e433b3d95a48
Sean M. Snyder <sean.snyder@snhu.edu>
2014-03-13 16:53:58 -0400

    boostrap working finally

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,7 +8,7 @@ module UsersHelper
   def mini_gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, size: '20x16', alt: user.name, class: "img-circle")
+    image_tag(gravatar_url, size: '20x16', alt: user.name, class: "img-mini-thumbnail")
   end
   def med_gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)

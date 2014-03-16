2a3f269bc48c9fff62390a21100ba538215fa042
Sean M. Snyder <sean.snyder@snhu.edu>
2014-03-16 19:11:00 -0400

    fixes

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,8 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
   validates :content, length: {minimum: 2, maximum: 100}
   validates :user_id, numericality: true
+
+  def self.recent
+    where('created_at > ?', 1.weeks.ago).pluck(:content, :user_id)
+  end
 end

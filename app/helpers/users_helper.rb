555b0ed897f073aa8605dde3acc2b69a26da3a43
Sean M. Snyder <sean.snyder@snhu.edu>
2014-03-16 01:47:16 -0400

    fixed show post page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -15,4 +15,9 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, size: '75x75', alt: user.name, class: "img-rounded")
   end
+  def show_gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, size: '50x50', alt: user.name, class: "img-thumbnail")
+  end
 end
